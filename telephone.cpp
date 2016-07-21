7c439569aad897287316782495bac3f93cd0198f
richardkarras <richard.karras@gmail.com>
2016-07-20 22:28:06 -0600

    dialer software / added clues from Robbie

--- telephone.cpp
+++ telephone.cpp
@@ -18,9 +18,12 @@ int main()
     //Define variables
     string phone_number[4];
     // 4 digit phone number, input from rotary phone
-    string hint_1[4], hint_2[4];
-    hint_1 = '4567';
-    hint_2 = '7654';
+    string code_1[4], code_2[4], code_3[4], code_4[4], code_5[4];
+    code_1 = '7513';
+    code_2 = '4900';
+	code_3 = '8020';
+	code_4 = '0918';
+    code_5 = '3855';
     
     while (1)
     {
@@ -30,13 +33,22 @@ int main()
         
         //switch/case build to evaulate incoming rf tag against desired audio out
         
-        if(strcmp(phone_number,hint_1)==0)
+        if(strcmp(phone_number,code_1)==0)
             audio_out(1);
         
-        else if (strcmp(phone_number,hint_2)==0)
+        else if (strcmp(phone_number,code_2)==0)
             audio_out(2);
         
-        else audio_out(3);
+		else if (strcmp(phone_number, code_3) == 0)
+			audio_out(3);
+
+		else if (strcmp(phone_number, code_4) == 0)
+			audio_out(4);
+
+        else if (strcmp(phone_number, code_5) == 0)
+            audio_out(5);
+            
+        else audio_out(6);
     }
     return 0;
 }
@@ -46,14 +58,32 @@ void audio_out(int case_number)
     switch (case_number)
     {
         case '1':
-            system("audio_script_1.sh");
+        {
+            system("canberra-gtk-play -f audio1.ogg");
             break;
-            
+        }
         case '2':
-            system("audio_script_2.sh");
+        {
+            system("canberra-gtk-play -f audio2.ogg");
             break;
-            
-        default: system("dial_tone.sh");
+        }
+		case '3':
+        {
+            system("canberra-gtk-play -f audio3.ogg");
+			break;
+        }
+
+		case '4':
+        {
+            system("canberra-gtk-play -f audio4.ogg");
+			break;
+        }
+        case '5':
+        {
+            system("canberra-gtk-play -f wincond.ogg");
+            break
+        }
+		default: system("canberra-gtk-play -f error.ogg");
             
     }//output audio queues based on incoming call number
     return;
